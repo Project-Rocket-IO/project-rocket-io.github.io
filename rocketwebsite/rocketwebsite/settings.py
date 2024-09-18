deffaf705fe46a4de0c4cdb1725fc5b00e2371a1
Johnny <johnvjlang@gmail.com>
2024-09-17 22:39:57 -0600

    database changes

--- rocketwebsite/rocketwebsite/settings.py
+++ rocketwebsite/rocketwebsite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,13 +85,10 @@ WSGI_APPLICATION = 'rocketwebsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {'default': dj_database_url.config(default='postgresql://postgres:mzQIRALoLwlJFTqbKdpMdtVbWNUBHndW@postgres-92nr.railway.internal:5432/railway')}
 
+# Enable Connection Pooling
+DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
