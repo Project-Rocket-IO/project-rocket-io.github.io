bc3e6705b312b35d5f2107dcfac3c85373560538
Johnny <johnvjlang@gmail.com>
2024-10-01 11:39:08 -0600

    updated static directory handling

--- rocketwebsite/website/urls.py
+++ rocketwebsite/website/urls.py
@@ -1,6 +1,8 @@
 # urls.py
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -9,5 +11,4 @@ urlpatterns = [
     path('blogs/', views.blog_list, name='blogs'),
     path('blog/<int:pk>', views.blog, name='blog'),
     path('pricing/', views.pricing, name='pricing'),
-]
-
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
